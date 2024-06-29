d6c3c8d0bf39ff13078fef7869ca6f1d54f7c574
Wade Johnson <wadej93@gmail.com>
2024-06-29 15:51:31 -0700

    add brew to path after install

--- run_once_before_install-brew.sh
+++ run_once_before_install-brew.sh
@@ -3,4 +3,8 @@
 if ! command -v brew &> /dev/null
 then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    # Next steps according to brew install script installation script
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/wjohnson/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
