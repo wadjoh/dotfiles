37ce19c5e0f6db0ac9669b6f1cf2456491dbba2c
Wade Johnson <wadej93@gmail.com>
2025-03-01 09:54:02 -0800

    split vscode brew bundle to other file

--- run_after_brew-bundle.sh
+++ run_after_brew-bundle.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-if command -v brew &> /dev/null
-then
+if command -v brew &> /dev/null; then
   brew bundle --global
+fi
+
+if command -v code &> /dev/null; then
+  brew bundle ~/.vscode.Brewfile
 fi
